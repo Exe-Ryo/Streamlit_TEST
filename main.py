d85b977d00c09e925a01860c94a3afc37e9e3b2f
Exe-Ryo <102363757+Exe-Ryo@users.noreply.github.com>
2022-07-25 22:48:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -27,11 +27,11 @@ def main():
         ax.plot(np_data[:,11],np_data[:,8] , label=fand[i])
         
     ax.legend(bbox_to_anchor=(0, 0.2), loc='upper left', borderaxespad=0, fontsize=6, prop = {"family" : "Meiryo"})
-    ax.set_title("評価額推移", fontname = 'Meiryo')
+    ax.set_title("test title", fontname = 'Meiryo')
     #ax.set_xticklabels(np_data[:,11], rotation= 90)
     # ax.set_xticks(rotation=90)
-    ax.set_ylabel("評価額[円]", fontname ='Meiryo')
-    ax.set_xlabel("取得日時", fontname ='Meiryo')
+    ax.set_ylabel("kakaku", fontname ='Meiryo')
+    ax.set_xlabel("nichiji", fontname ='Meiryo')
     #ax.set_ylim(['5,000','25,000'])
     ax.grid(True) # (5)目盛線表示
     
